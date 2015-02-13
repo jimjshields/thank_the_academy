5345bd12d2c7c2dfecd8feae37a6a790ded698cb
jimjshields <jim.j.shields@gmail.com>
2015-02-13 16:28:04 -0500

    Split out into functions. Added profiling. Only call create_markov_gen once.

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 from flask import Flask, render_template, redirect, url_for, request
 from markov import MarkovGenerator
+import time
+
+def timefunc(f):
+	def f_timer(*args, **kwargs):
+		start = time.time()
+		result = f(*args, **kwargs)
+		end = time.time()
+		print f.__name__, 'took', end - start, 'time'
+		return result
+	return f_timer
 
 app = Flask(__name__)
 
 import csv
 
-speeches = []
+@timefunc
+def get_csv_data():
+	speeches = []
+	with open('speeches.csv', 'rU') as csv_file:
+		data = csv.reader(csv_file)
+		for row in data:
+			speeches.append(row)
+	return speeches
 
-with open('speeches.csv', 'rU') as csv_file:
-	data = csv.reader(csv_file)
-	for row in data:
-		speeches.append(row)
+@timefunc
+def get_only_speeches(speech_data):
+	only_speeches = map(lambda speech: speech[6], speech_data)
+	return only_speeches
 
+@timefunc
+def create_markov_gen(all_speeches):
+	markov_gen = MarkovGenerator(all_speeches, 1500, 10)
+	return markov_gen
 
-speeches = map(lambda speech: speech[6], speeches)
+@timefunc
+def generate_markov_words(markov_gen):
+	new_speech = markov_gen.generate_words().replace('  ', '@').replace(' ', '').replace('@', ' ')
+	return new_speech
 
-all_speeches = ' '.join(speeches)
+speech_data = get_csv_data()
+only_speeches = get_only_speeches(speech_data)
+all_speeches = ' '.join(only_speeches)
+markov_gen = create_markov_gen(all_speeches)
+new_speech = generate_markov_words(markov_gen)
+
 
 @app.route('/')
 def index():
-	markov_gen = MarkovGenerator(all_speeches, 1500, 10)
-	speech = markov_gen.generate_words().replace('  ', '@').replace(' ', '').replace('@', ' ')
-	return render_template('index.html', speech=speech)
+	new_speech = generate_markov_words(markov_gen)
+	return render_template('index.html', speech=new_speech)
 
 @app.route('/make_speech', methods=['POST', 'GET'])
 def make_speech():
@@ -29,4 +57,4 @@ def make_speech():
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
